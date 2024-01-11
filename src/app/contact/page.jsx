ae0d0000e4598687c7f59e0bd6d5491687c69268
KaranGulve4342 <karangulve4342@gmail.com>
2024-01-11 19:03:40 +0530

    Hydration test completed + localhost working

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,23 +1,45 @@
 "use client"
 
+// import { useEffect, useState } from 'react';
+// import HydrationTest from '@/components/hydrationTest'
+// import dynamic from 'next/dynamic'
 import styles from './contact.module.css'
 import Image from 'next/image'
 
+// const HydrationTestNoSSR = dynamic(() => import("@/components/hydrationTest"), {ssr: false})
+
 
 const ContactPage = () => {
-  console.log("It works here");
+  // console.log("It works here");
+  // const [isClient, setIsClient] = useState(false);
+
+  
+  // useEffect(() => {
+  //   setIsClient(true);
+  // }, []);
+
+  const a = Math.random();
+  
+  console.log(a);
+
+  
+
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
         <Image src="/contact.png" alt="" fill className={styles.img} />
       </div>
       <div className={styles.formContainer}>
+        {/* {isClient && a} */}
+        {/* < HydrationTestNoSSR /> */}
+        <div suppressHydrationWarning>{a}</div>
         <form action="" className={styles.form}>
           <input type="text" placeholder="Name and Surname" />
           <input type="text" placeholder="Email Address" />
           <input type="text" placeholder="Phone Number (Optional)" />
           <textarea name="" id="" cols="30" rows="10" placeholder="Message"></textarea>
-          <button onClick={()=>console.log("clicked")}>Send</button>
+          {/* <button onClick={()=>console.log("clicked")}>Send</button> */}
+          <button>Send</button>
         </form>
       </div>
     </div>
