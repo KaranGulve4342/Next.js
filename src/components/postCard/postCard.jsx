292c26803cedc8800e24a8a340a7b8ae3a7ec0c3
KaranGulve4342 <karangulve4342@gmail.com>
2024-01-12 17:41:37 +0530

    postCard completed

--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -1,25 +1,23 @@
-import Image from "next/image";
-import styles from "./postCard.module.css";
-import Link from "next/link";
+import Image from "next/image"
+import styles from "./postCard.module.css"
+import Link from "next/link"
 
-const postCard = ({post}) => {
+const PostCard = ({post}) => {
   return (
     <div className={styles.container}>
-        <div className={styles.top}>
-            <div className={styles.imgContainer}>
-                {/* <Image src="/post.png" alt="" fill className={styles.img} /> */}
-                <Image src="https://images.pexels.com/photos/1612351/pexels-photo-1612351.jpeg?auto=compress&cs=tinysrgb&w=900" alt="" fill className={styles.img} />
-            </div>
-            <span className={styles.date}>01.01.2024</span>
-        </div>
-        <div className={styles.bottom}>
-            <h1 className={styles.title}>{post.title}</h1>
-            <p className={styles.desc}>{post.body} </p>
-            {/* <Link className={styles.link} href="/blog/post">READ MORE</Link> */}
-            <Link className={styles.link} href={`/blog/${post.id}`}>READ MORE</Link>
-        </div>
+      <div className={styles.top}>
+        {post.img && <div className={styles.imgContainer}>
+          <Image src={post.img} alt="" fill className={styles.img}/>
+        </div>}
+        <span className={styles.date}>{post.createdAt?.toString().slice(4, 16)}</span>
+      </div>
+      <div className={styles.bottom}>
+        <h1 className={styles.title}>{post.title}</h1>
+        <p className={styles.desc}>{post.body}</p>
+        <Link className={styles.link} href={`/blog/${post.slug}`}>READ MORE</Link>
+      </div>
     </div>
   )
 }
 
-export default postCard
+export default PostCard
